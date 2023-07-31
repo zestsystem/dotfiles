28d8150166375a554b11c6c891ac21e4e5fd8b84
Mike Yim <mikeyim@unitetheculture.org>
2023-07-30 17:13:00 -0700

    fix: enable experimantal flake

--- system/darwin/home-manager.nix
+++ system/darwin/home-manager.nix
@@ -1,15 +1,8 @@
-{ inputs }:
-
-{ pkgs, ... }:
-
-let
-  shared-config = import ../shared/home-manager.nix { inherit inputs; };
-  shared-packages = import ../shared/home-manager-packages.nix { inherit pkgs; };
-in
-{
-  imports = [ shared-config ];
-
-  home.file."Library/Application Support/k9s/skin.yml".source = ../../config/k9s/skin.yml;
+{inputs}: {pkgs, ...}: let
+  shared-config = import ../shared/home-manager.nix {inherit inputs;};
+  shared-packages = import ../shared/home-manager-packages.nix {inherit pkgs;};
+in {
+  imports = [shared-config];
 
   home.packages = shared-packages;
 }
