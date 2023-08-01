c3391c6bfc86a2b9d58568efdea1de802e47df32
Erik Reinert <mikeyim@unitetheculture.org>
2023-07-31 19:14:41 -0700

    remove unused bindings

--- system/shared/home-manager.nix
+++ system/shared/home-manager.nix
@@ -6,16 +6,6 @@
     sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
   };
   isDarwin = pkgs.system == "aarch64-darwin" || pkgs.system == "x86_64-darwin";
-  jsonnet = import ./jsonnet.nix {inherit pkgs;};
-  vim-just = pkgs.vimUtils.buildVimPlugin {
-    name = "vim-just";
-    src = pkgs.fetchFromGitHub {
-      owner = "NoahTheDuke";
-      repo = "vim-just";
-      rev = "10de9ebf0bd8df8ff8593b0b87ec8bf3b715326f";
-      sha256 = "sha256-NGhWF4/SEPww9e/wCDghGMSPZmmAbms6tn/IHqDMDkI=";
-    };
-  };
   zsh-z = pkgs.fetchFromGitHub {
     owner = "agkozak";
     repo = "zsh-z";
@@ -183,7 +173,6 @@ in {
       };
     };
   };
-
 
   programs.nnn = {
     enable = true;
