c6452afd99d5a7cf74d1526ef347f4bdb611717d
zestsystem <mikeyim@unitetheculture.org>
2023-08-14 21:24:11 -0700

    feat: switch to prettierd from prettier

--- config/nvim/lua/zestsystem/plugins/lsp/formatting/prettier.lua
+++ config/nvim/lua/zestsystem/plugins/lsp/formatting/prettier.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"williamboman/mason.nvim",
 		opts = function(_, opts)
-			table.insert(opts.ensure_installed, "prettier")
+			table.insert(opts.ensure_installed, "prettierd")
 		end,
 	},
 	{
