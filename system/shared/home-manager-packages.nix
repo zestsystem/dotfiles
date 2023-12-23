491df02387dba251ccec1b89bdc8d39fe16e979b
zestsystem <mikeyim@unitetheculture.org>
2023-12-22 22:51:40 -0800

    feat: remove unused software

--- system/shared/home-manager-packages.nix
+++ system/shared/home-manager-packages.nix
@@ -1,19 +1,11 @@
 {pkgs, ...}:
 with pkgs; [
-  awscli2
-  azure-cli
   doppler
   fd
   fzf
   gh
-  google-cloud-sdk
   jq
   just
-  k9s
-  kind
-  kubectl
   nurl
-  pulumi-bin
   ripgrep
-  terraform
 ]
